e17de8db681efbed6b2ac5e312d318c5daf39bd7
Parashara <parasharar@gmail.com>
2024-10-30 15:46:24 +0800

    changes to question 2

--- Robust/robust_aggregator.py
+++ Robust/robust_aggregator.py
@@ -11,6 +11,7 @@ from tqdm import tqdm
 def robust_aggregator(gradients,
                       eps_threshold=9 * 39275):  # hardcoding the eps threshold to k* SIGMA provided in question
     # Clone the original gradients to avoid modifying the input
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     gradients = gradients.clone()
     n = gradients.shape[0]
 
@@ -26,14 +27,16 @@ def robust_aggregator(gradients,
             lambdas, U = torch.linalg.eig(cov)
 
             # need to compare with the spectral norm which corresponds to the largest eigen value  which is in the first position (based on the provided algorithm )
-            if torch.abs(lambdas[0]).item() >= eps_threshold:  # taking abs because the eigen value can be complex
+            spectral_norm = torch.abs(lambdas[0]).item() # taking abs because the eigen value can be complex
+
+            if spectral_norm > eps_threshold:
                 mean_gradient = gradients.mean(dim=0)
 
                 # Project the distance of every gradient wrt mean gradient along the max variance direction
                 diff_gradient = gradients - mean_gradient
                 diff_gradient = diff_gradient.to(torch.complex64)
                 max_var_eigen_vector = U[0]
-                projections_on_max_var = diff_gradient @ max_var_eigen_vector
+                projections_on_max_var = diff_gradient.to(device) @ max_var_eigen_vector.to(device)
 
                 # Find the index of the gradient with the maximum absolute distance
                 outlier_index = torch.abs(projections_on_max_var).argmax()
@@ -54,23 +57,43 @@ def robust_aggregator(gradients,
     # return the pruned gradients
     return gradients
 
-# TODO.x does not result in a PSD matrix as there are complex values... need to fix it
 def calculate_covariance_matrix(X):
     ''' This calculates the covariance matrix in the same way prescribed in the slides without using a direct cov function present in torch'''
-    # X = X.clone()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    X = X.to(device)
     n = X.shape[0]
-    mu = X.mean(dim=0, keepdim=True)  # Shape (1, d)
+    d = X.shape[1]
+    mu = X.mean(dim=0)  # Shape (d,)
     X = X - mu  # Shape (n, d) - centered along the mean
-    cov_matrix = (X.T @ X) / (n - 1)  # cov matrix of shape d,d
+
+    # approach 1. possibly incorrect as this will make X[i]
+    # cov_matrix = (X.T @ X) / (n - 1)  # cov matrix of shape d,d
+
+    # approach 2: the dot product is calculated for each X[i] wrt itself and then summed and divided as opposed to approach 1 since there are differences
+    cov_matrix = torch.zeros((d,d)).to(device)
+    for i in range(n):
+        x = X[i].unsqueeze(0) # shape (1,d)
+        xt = x.transpose(0,1) # shape (d,1)
+        cov_matrix += xt @ x # shape (d,d)
+
+    cov_matrix /= n-1
+
+    # approach 3: matrix form of doing approach 2 (but still not getting the exact same answer) and was slow on cpu
+    # X_centered_expanded = X.unsqueeze(1)  # Shape (n, 1, d)
+    # outer_products = X_centered_expanded.transpose(1, 2) @ X_centered_expanded # Shape (n, d, d)
+    # cov_matrix_other = outer_products.sum(dim=0) / (n - 1)  # Shape (d, d)
+
+    # is_psd = is_matrix_psd(cov_matrix)
     return cov_matrix
 
 def is_matrix_psd(X):
-    eigenvalues, _ = torch.linalg.eig(X)
+    eigenvalues, eigenvectors = torch.linalg.eig(X)
     return torch.all(eigenvalues.real >= -1e-10).item()
 
 if __name__ == '__main__':
     # Setup
     torch.set_printoptions(precision=10, sci_mode=True)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # shape is (1545, 2, 768)
     gradients = torch.load("all_gradients.pt")
@@ -93,12 +116,12 @@ if __name__ == '__main__':
 
     # step1: max value of covariance
     max_cov = cov.max()
-    print(f"max covariance is {max_cov}")
+    print(f"max covariance is {max_cov}") # 39616.3515625
     max_cov_ind = cov.argmax()
 
     # step2: Convert the linear index to 2D coordinates (i, j)
     i, j = divmod(max_cov_ind.item(), d)
-    print(f"max cov index is {i}, {j}")
+    print(f"max cov index is {i}, {j}") #296,296
 
     # step3: what is the direction of the max variance (unit vector along max eigen vector), since cov is positive semi definite no need to sort as the first eigen value is already max
     lambdas, U = torch.linalg.eig(cov)
@@ -106,8 +129,8 @@ if __name__ == '__main__':
     norm = torch.norm(max_var_eigen_vector, p=2)
     max_var_direction = max_var_eigen_vector / norm  # shape (d,)
     print(f"max variance eigen vector is {max_var_direction}")
-    print(f"first 3 values of max var direction is [{max_var_direction[:3]}]")
-    print(f"last 3 values of max var direction is [{max_var_direction[-3:]}]")
+    print(f"first 3 values of max var direction is [{max_var_direction[:3]}]") #[tensor([-3.6010164768e-02+0.j, 3.4746624529e-02+0.j, 2.3515963927e-02+0.j],device='cuda:0')]
+    print(f"last 3 values of max var direction is [{max_var_direction[-3:]}]") #[tensor([ 3.4827093032e-07+0.j,  1.2595599230e-07+0.j, -9.0192429525e-08+0.j],device='cuda:0')]
     print("-" * 60)
     print()
 
@@ -124,15 +147,15 @@ if __name__ == '__main__':
     diff_gradient = gradients - mean_gradient  # shape (n,d)
     diff_gradient = diff_gradient.to(
         torch.complex64)  # making it a complex tensor because the max var direction contains imaginary components shape (n,d)
-    projections_on_max_var = diff_gradient @ max_var_direction  # shape (n,)
+    projections_on_max_var = diff_gradient.to(device) @ max_var_direction.to(device)  # shape (n,)
 
     # step3: Find the index of the gradient with the maximum absolute distance
     outlier_index = torch.abs(projections_on_max_var).argmax()
     outlier_gradient = gradients[outlier_index]
 
-    print(f"The most likely outlier gradient index is: {outlier_index}")
-    print(f"First 3 values of the outlier gradient: {outlier_gradient[:3]}")
-    print(f"Last 3 values of the outlier gradient: {outlier_gradient[-3:]}")
+    print(f"The most likely outlier gradient index is: {outlier_index}") #872
+    print(f"First 3 values of the outlier gradient: {outlier_gradient[:3]}") #tensor([-0.0000000000e+00, 2.4866737425e-02, -1.1044409275e+00])
+    print(f"Last 3 values of the outlier gradient: {outlier_gradient[-3:]}") #tensor([1.0991185904e+00, -2.6810422540e-01, 7.0607316494e-01])
     print("-" * 60)
     print()
 
@@ -148,11 +171,11 @@ if __name__ == '__main__':
 
     # Step 3: Determine the maximum variance from the new covariance matrix
     new_max_cov = new_cov.max()
-    print(f"New maximum covariance among remaining gradients is: {new_max_cov}")
+    print(f"New maximum covariance among remaining gradients is: {new_max_cov}") # 39492.1875
 
     # Step 4: Calculate the change in maximum variance
     variance_change = new_max_cov - max_cov
-    print(f"Change in maximum variance: {variance_change}")
+    print(f"Change in maximum variance: {variance_change}") # -124.1640625 , it has decreased from old value
     print("-" * 60)
     print()
 
@@ -163,4 +186,4 @@ if __name__ == '__main__':
 
     pruned_gradients = robust_aggregator(gradients)
     num_poisoned = gradients.shape[0] - pruned_gradients.shape[0]
-    print(f"percentage of poisoned gradients: {(num_poisoned / 673) * 100} % ") # got 82.61% (afer 18 mins) i.e. 556/673
+    print(f"percentage of poisoned gradients: {(num_poisoned / 673) * 100} % ") # got 82.76374442793461% (afer 18 mins) i.e. 557/673
